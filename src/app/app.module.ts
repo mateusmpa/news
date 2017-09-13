1bf82fd388c9d3ddd76f6687e4f1e25eb9a2ad49
Mateus Alves <mateus.alves@locaweb.com.br>
2017-09-12 21:00:59 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './news/main/main.component';
+import { NewComponent } from './news/new/new.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'new', component: NewComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NewComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule
   ],
   providers: [],
